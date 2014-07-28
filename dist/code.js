f4c8b76a42f5629f730b5628e1d09dbddc48c3c0
Ikuo Terado <eqobar@gmail.com>
2014-07-29 02:41:05 +0900

    Update dist

--- dist/code.js
+++ dist/code.js
@@ -1,10 +1,18 @@
-var checkData, doGet;
+var addEvent, checkData, doGet, removeEvent;
 
 doGet = function(e) {
   return HtmlService.createHtmlOutputFromFile('index');
 };
 
 checkData = function(form) {
+  if (form.cmd === 'delete') {
+    return removeEvent(form);
+  } else {
+    return addEvent(form);
+  }
+};
+
+addEvent = function(form) {
   var cal, event, events, mail, name, resource, time_e, time_s, _i, _len;
   name = form.name;
   mail = form.mail;
@@ -24,4 +32,23 @@ checkData = function(form) {
     guests: mail
   });
   return '予約しました。(ブラウザーをリロードすると、カレンダーが更新されます)';
+};
+
+removeEvent = function(form) {
+  var cal, event, events, mail, name, resource, time_e, time_s, _i, _len;
+  name = form.name;
+  mail = form.mail;
+  time_s = new Date(form.time_s);
+  time_e = new Date(form.time_e);
+  resource = form.resource;
+  cal = CalendarApp.getCalendarById('__ID__');
+  events = cal.getEvents(time_s, time_e);
+  for (_i = 0, _len = events.length; _i < _len; _i++) {
+    event = events[_i];
+    if (event.getTitle() === resource && event.getDescription() === ("" + name + " (" + mail + ")")) {
+      event.deleteEvent();
+      return '削除しました。(ブラウザーをリロードすると、カレンダーが更新されます)';
+    }
+  }
+  return '該当する予約がなかったため、削除できませんでした。';
 };
